befc6402a39ed3a5888df02b65164a6d8ba9fe1c
Josh Archer <jarcher@greenriver.edu>
2020-04-11 22:32:17 -0700

    Adding initial files to the project.

---
+++ scripts/tickets.js
@@ -0,0 +1,15 @@
+/*
+    This file calculates the cost of theme park tickets when
+    the user clicks the calculate button.
+    Author:
+    File:
+    Date:
+*/
+
+let button = document.querySelector("button");
+button.onclick = function(event) {
+    //prevent the form from submitting
+    event.preventDefault();
+
+    //your code goes here...
+}
